a6af237c9867f59268d4dcf44cddbacd5763d6dd
Akanksha kumari <akankshakumari393@gmail.com>
2023-06-15 20:02:24 +0530

    Time: 22 ms (30.47%), Space: 44.6 MB (70.67%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -23,13 +23,12 @@ public class Codec {
                 sb.append("# ");
                 // imp
                 continue;
-            } 
+            }
             // adding space because there may be negative integers as well
             sb.append(node.val + " ");
             queue.add(node.left);
             queue.add(node.right);
         }
-        System.out.println(sb.toString());
         return sb.toString();
     }
 
@@ -40,18 +39,18 @@ public class Codec {
         // splitting as string[] and not char because of negative integer case
         String[] values = data.split(" ");
         TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        q.add(root);        
+        q.add(root);
         for (int i = 1; i < values.length; i++) {
             TreeNode parent = q.poll();
             // add left child
-            if(!values[i].equals("#")) {
+            if (!values[i].equals("#")) {
                 TreeNode left = new TreeNode(Integer.parseInt(values[i]));
                 parent.left = left;
                 q.add(left);
             }
             i++;
             // add right child
-            if( i < values.length && !values[i].equals("#")) {
+            if (i < values.length && !values[i].equals("#")) {
                 TreeNode right = new TreeNode(Integer.parseInt(values[i]));
                 parent.right = right;
                 q.add(right);
@@ -60,7 +59,6 @@ public class Codec {
         return root;
     }
 }
-
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
